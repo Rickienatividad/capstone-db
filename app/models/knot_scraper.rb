61449c65af068e8df2f9fde9df648ac984264cdb
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-10 17:01:51 -0500

    redoing knot_scraper.rb

--- app/models/knot_scraper.rb
+++ app/models/knot_scraper.rb
@@ -3,11 +3,11 @@ require "pry"
 require "httparty"
 
 class Knot_Scraper
-  url = "https://www.mossyoak.com/our-obsession/blogs/fishing/five-knots-every-fisherman-should-know"
-
+  url = ""
   knot_list = HTTParty.get(url)
 
   doc = Nokogiri::HTML(knot_list)
+
   binding.pry
 end
 
