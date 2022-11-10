cb0e4b0a8d7272da2af2ca2ad206b90d00d11dde
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-10 17:14:56 -0500

    provides knot_scraper with urls to sample knots

--- app/models/knot_scraper.rb
+++ app/models/knot_scraper.rb
@@ -3,11 +3,18 @@ require "pry"
 require "httparty"
 
 class Knot_Scraper
-  url = ""
+  url = "https://www.fishingloft.com/fishing-knots.html"
   knot_list = HTTParty.get(url)
 
   doc = Nokogiri::HTML(knot_list)
 
+  knots = doc.css("p").css("a").first(5)
+
+  knot_urls = []
+  knots.each do |knot|
+    url = knot["href"]
+    knot_urls << url
+  end
   binding.pry
 end
 
