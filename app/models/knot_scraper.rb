7b23dd1b7718800d83efbb034f4f2ccc71a8c608
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-10 19:48:06 -0500

    knot urls added to knot_scraper

--- app/models/knot_scraper.rb
+++ app/models/knot_scraper.rb
@@ -3,19 +3,31 @@ require "pry"
 require "httparty"
 
 class Knot_Scraper
-  url = "https://www.fishingloft.com/fishing-knots.html"
+  # def scrape_knot_urls
+  url = "https://www.101knots.com/category/fishing-knots"
   knot_list = HTTParty.get(url)
 
   doc = Nokogiri::HTML(knot_list)
 
-  knots = doc.css("p").css("a").first(5)
-
   knot_urls = []
+
+  knots = doc.css(".columnthree").css("ul").css("li").css("a")
+
   knots.each do |knot|
-    url = knot["href"]
+    url = knot.attributes["href"].value
     knot_urls << url
   end
+
   binding.pry
+
+  scrape_knot_pages(knot_urls)
+
+  def scrape_knot_pages(knot_urls)
+    knot_urls.each do |url|
+      html = HTTParty.get(url)
+      doc = Nokogiri::HTML(html)
+    end
+  end
 end
 
 knot_scraper = Knot_Scraper.new
