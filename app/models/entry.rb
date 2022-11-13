ae3d79dfdb158c9f5e33e3396cbc24f661eb1025
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-13 18:47:05 -0500

    adds validations to entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,8 @@ class Entry < ApplicationRecord
   belongs_to :user
   has_many :fish
   has_one :lure
+
+  validates :date, presence: true
+  validates :weather, presence: true
+  validates :location, presence: true
 end
