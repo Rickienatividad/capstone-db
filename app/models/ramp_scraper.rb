362142a2349cbe040329a9a45780caf6c2756fcc
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-13 23:05:08 -0500

    scrapes list of ramps

--- app/models/ramp_scraper.rb
+++ app/models/ramp_scraper.rb
@@ -1,6 +1,7 @@
 require "httparty"
 require "nokogiri"
 require "pry"
+require "open-uri"
 
 class Ramp_scraper
   def scrape_lake_list
@@ -16,10 +17,33 @@ class Ramp_scraper
       lake_url = "https://www.dnr.sc.gov/lakes/" + lake
       lake_array << lake_url
     end
+    lake_array.shift
     scrape_urls(lake_array)
   end
 
   def scrape_urls(lake_array)
+    ramp_list = []
+    lake_array.each do |lake|
+      html = HTTParty.get(lake)
+      doc = Nokogiri::HTML(html)
+
+      ramps = doc.css(".twoColumn").css("strong")[5].css("a")
+      ramp_list << ramps
+    end
+    url_list = []
+    ramp_list.each do |ramp|
+      url = ramp.attr("href")
+      url_list << url
+      url_list.delete(nil)
+    end
+    link_list = []
+    url_list.each do |url|
+      link = url.value
+      link_list << link
+    end
+    link_list.delete_at(4)
+    link_list.pop
+    binding.pry
   end
 end
 
